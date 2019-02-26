2070087aae60890ce384f3956d610d75ef6ecce8
Jordan Beaman <Jordanbeaman94@gmail.com>
2019-02-25 19:03:48 -0600

    Updates

---
+++ precache-manifest.761d9899ea3fddf058edf283b75b2c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Expense-Tracker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0e6b955e7368d0ddc2f5",
+    "url": "/Expense-Tracker/static/js/runtime~main.0e6b955e.js"
+  },
+  {
+    "revision": "ecde74d04f8a70dc9820",
+    "url": "/Expense-Tracker/static/js/main.ecde74d0.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Expense-Tracker/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "ecde74d04f8a70dc9820",
+    "url": "/Expense-Tracker/static/css/main.59e449a0.chunk.css"
+  },
+  {
+    "revision": "06ea670a76d8bf346018275597ddb54c",
+    "url": "/Expense-Tracker/index.html"
+  }
+];
